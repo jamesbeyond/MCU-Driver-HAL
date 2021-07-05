8f371ccb8fe11eb7f2739c3dd98a64f51e18a5cb
Qinghao Shi <qinghao.shi@arm.com>
2021-07-05 14:41:20 +0100

    fake a license

--- wronglicense/PGL_llicense.h
+++ wronglicense/PGL_llicense.h
@@ -1,24 +1,18 @@
-/* Declarations for the parser for C and Objective-C.
-   Copyright (C) 1987-2021 Free Software Foundation, Inc.
-
-   Parser actions based on the old Bison parser; structure somewhat
-   influenced by and fragments based on the C++ parser.
-
-This file is part of GCC.
-
-GCC is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation; either version 3, or (at your option) any later
-version.
-
-GCC is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
-
-You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING3.  If not see
-<http://www.gnu.org/licenses/>.  */
+/* Copyright (c) 2020-2021 Arm Limited
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef GCC_C_PARSER_H
 #define GCC_C_PARSER_H
